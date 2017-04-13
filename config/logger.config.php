95c3b490a2592554b591dcb6778169f18659a94a
Daniel Gadd <DJGadd@DJGadd.local>
2017-04-13 14:48:11 +0100

    * Using env function for easier defaults * Adding a slack-username key to specify the name that the slack bot should post as

--- config/logger.config.php
+++ config/logger.config.php
@@ -7,7 +7,7 @@ return [
   |--------------------------------------------------------------------------
   | The channel to log to (this is unrelated to the slack-channel key)
   */
-  'channel' => getenv('ENVIRONMENT'),
+  'channel' => env('ENVIRONMENT', 'production'),
 
   /*
   |--------------------------------------------------------------------------
@@ -16,7 +16,7 @@ return [
   | Enabling this will wrap all handlers in a FingersCrossedHandler (so you won't
   | get verbose logging unless an error is triggered.)
   */
-  'environment' => getenv('ENVIRONMENT'),
+  'environment' => env('ENVIRONMENT', 'production'),
 
   /*
   |--------------------------------------------------------------------------
@@ -33,14 +33,19 @@ return [
   |--------------------------------------------------------------------------
   | Your loggly API token, unset or leave blank to ignore
   */
-  'loggly-token' => '',
+  'loggly-token' => env('LOGGLY_TOKEN'),
 
   /*
   |--------------------------------------------------------------------------
   | SlackHandler
   |--------------------------------------------------------------------------
-  | Your Slack API token and channel, unset or leave blank to ignore
+  | Your Slack API token, channel, and bot username, unset or leave blank to
+  | to disable. Your slack-username key can be anything you like, if you have
+  | multiple websites dumping logs into the same channel we'd recommend setting
+  | it to something to specific to the site rather than the generic 'Logger'
+  | default
   */
-  'slack-token' => '',
-  'slack-channel' => '#logs',
+  'slack-token' => env('SLACK_TOKEN'),
+  'slack-channel' => env('SLACK_CHANNEL', '#logs'),
+  'slack-username' => env('SLACK_USERNAME', 'Logger'),
 ];
